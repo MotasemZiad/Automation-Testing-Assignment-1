3cead692dcedf1b96dd1dfeabbe726154a839b07
Motasem Abunima <motasemabunima@gmail.com>
2023-10-03 10:27:47 +0300

    using the annotation methods

--- src/test/java/AnnotationTest.java
+++ src/test/java/AnnotationTest.java
@@ -1,7 +1,58 @@
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 public class AnnotationTest {
+
+    @BeforeMethod
+    public void printBeforeMethod(ITestResult result) {
+        System.out.println("BeforeMethod: " + result.getMethod().getMethodName());
+    }
+
+    @BeforeClass
+    public void printBeforeClass() {
+        System.out.println("BeforeClass");
+    }
+
+    @BeforeGroups
+    public void printBeforeGroups() {
+        System.out.println("BeforeGroups");
+    }
+
+    @BeforeTest
+    public void printBeforeTest() {
+        System.out.println("BeforeTest");
+    }
+
+    @BeforeSuite
+    public void printBeforeSuite() {
+        System.out.println("BeforeSuite");
+    }
+
+    @AfterMethod
+    public void printAfterMethod(ITestResult result) {
+        System.out.println("AfterMethod: " + result.getMethod().getMethodName());
+    }
+
+    @AfterClass
+    public void printAfterClass() {
+        System.out.println("AfterClass");
+    }
+
+    @AfterGroups
+    public void printAfterGroups() {
+        System.out.println("AfterGroups");
+    }
+
+    @AfterTest
+    public void printAfterTest() {
+        System.out.println("AfterTest");
+    }
+
+    @AfterSuite
+    public void printAfterSuite() {
+        System.out.println("AfterSuite");
+    }
 
     @Test(priority = 1)
     public void test1() {
